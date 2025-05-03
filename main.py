2d6b382ac0f33fc9b9fda4ce449770680764d154
aaryapawar05 <aaryapawar63@gmail.com>
2025-05-03 20:48:06 +0530

    Add main.py with band name generator code

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+a =input("whats the name of the city you grew up in =\n")
+b =input("Whats your pet's name =\n")
+print("your band name could be = "   +  a + " " + b)
